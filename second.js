42c9357c66a0aab226b8a3a48ada74dee0f66664
ShivamJaiswal2006101 <90027183+ShivamJaiswal2006101@users.noreply.github.com>
2021-11-01 19:12:16 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,16 @@
+var x="Shivam";
+console.log(x);
+var a=function ()
+{
+	var x=Question-02;
+	console.log(x);
+	b();
+	function b(){
+		console.log(x);}
+	c();
+}
+function c()
+{
+	console.log(x);
+}
+a();
